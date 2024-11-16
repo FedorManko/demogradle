34288c918e1eca5ca93adcc12b63698334c49fb1
FedorManko <tfma94@gmail.com>
2024-11-16 09:52:53 +0300

    second commit docker fix

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String hello(){
-        return "Hello World";
+    public String hello(@Value("${value.name}") String name) {
+        return "Hello World "  + name;
     }
 
 }
